ac5288b03776dfcaeffcc2cee99c9d9fea3b6ab4
Jamil Campeão <jamilcampeao@gmail.com>
2025-03-18 10:48:15 -0300

    Ajuste de segurança

--- server.js
+++ server.js
@@ -5,8 +5,6 @@ const { PrismaClient } = require("@prisma/client");
 const app = express();
 const prisma = new PrismaClient();
 
-app.use(cors());
-
 app.use(express.json());
 app.use(cors({origin: '*'}));
 
